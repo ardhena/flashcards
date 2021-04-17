add16f77cd39fc46c545240aeba99466e90e9692
Olga Draber <olgadraber@gmail.com>
2021-04-17 16:10:44 +0200

    remove old rails project

--- app/controllers/sets_controller.rb
+++
@@ -1,5 +0,0 @@
-class SetsController < ApplicationController
-  def show
-    @set = Set::Get.(id: params[:id])[:data]
-  end
-end
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get "/", to: "repositories#index"
-
-  resources :repositories, only: [:show, :index]
-  resources :sets, only: [:show]
-end
