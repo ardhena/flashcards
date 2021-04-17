1f42bc19f34f75a56419c1cda2a5e56204cad7ff
Olga Draber <olgadraber@gmail.com>
2021-04-17 19:46:37 +0200

    specify repository path for each env

--- lib/domains/flashcards/sets.rb
+++ lib/domains/flashcards/sets.rb
@@ -1,21 +1,22 @@
 class Flashcards::Sets
   class << self
-
-    REPO_PATH = "./tmp/flashcards_repository".freeze
-
     def create(name, content)
-      File.open("#{REPO_PATH}/#{name}", 'w') { |file| file.write(content) }
+      File.open("#{repo_path}/#{name}", 'w') { |file| file.write(content) }
     end
 
     def find(name)
-      content = File.open("#{REPO_PATH}/#{name}", 'r').read
+      content = File.open("#{repo_path}/#{name}", 'r').read
       Flashcards::Set.new(name, content)
     end
 
     def all
-      Dir.entries(REPO_PATH).reject{ |name| name == "." || name == ".." }
+      Dir.entries(repo_path).reject{ |name| name == "." || name == ".." }
                             .sort
                             .map{ |name| Flashcards::Set.new(name) }
     end
+
+    private
+
+    def repo_path = Settings.flashcards_repository_path
   end
 end
